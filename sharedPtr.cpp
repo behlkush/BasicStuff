d04b75292d73e987283961c532c67ff78540a03c
behlkush <behlkush@gmail.com>
2016-05-19 17:09:44 -0700

    Update sharedPtr.cpp

--- sharedPtr.cpp
+++ sharedPtr.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <memory>
-
+//First edit of sharedPtr to check git repository working
 int main(){ 
 	std::shared_ptr<int> sp(new int(5));
 	std::cout << "Value is: " << *sp << std::endl;
